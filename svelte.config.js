25822fa61e5165734a60ac26e8a3a1e583174b68
Moueen-Togarvi <moueenuddintogarvi41@gmail.com>
2026-03-04 20:45:09 +0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
+
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      // Disable precompression of all assets in the output folder
-      precompress: false,
-      // Strict mode for edge runtime
-      strict: false
-    })
+    adapter: adapter()
   }
 };
 
